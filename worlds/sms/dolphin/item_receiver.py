7f9ccfcb92e381821ddebb34a02a872e9c6c4ad8
Marina <philnschipper@gmail.com>
2024-01-15 10:00:11 -0500

    Battery's last legs

--- worlds/sms/dolphin/item_receiver.py
+++ worlds/sms/dolphin/item_receiver.py
@@ -1,12 +1,16 @@
 import dolphin_memory_engine as dme
+import addresses
 
-def unpack_item(int item)
-# this is for testing only, I'll write a real function later, don't arrest me code police
-    if(item == 523004)
-        shineCountAdd()
-    return;
 
-def shineCountAdd()
-    value = dme.read_byte(SMS_SHINE_COUNTER)
+def shine_count_add():
+    value = dme.read_byte(addresses.SMS_SHINE_COUNTER)
     value += 1
-    dme.write_byte(SMS_SHINE_COUNTER, value)
+    dme.write_byte(addresses.SMS_SHINE_COUNTER, value)
+
+
+def unpack_item(item):
+    # this is for testing only, I'll write a real function later, don't arrest me code police
+    if item == 523004:
+        shine_count_add()
+    return
+
