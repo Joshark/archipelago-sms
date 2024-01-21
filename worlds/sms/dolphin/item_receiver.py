3eccd8855a7f8973834e956163afa9badae49b64
Marina <philnschipper@gmail.com>
2024-01-21 11:53:14 -0500

    Pre-nozzle functions
    Probably broken for now, wrote a bunch of functions and don't remember what order to call them. I need sleep

--- worlds/sms/dolphin/item_receiver.py
+++ worlds/sms/dolphin/item_receiver.py
@@ -5,7 +5,8 @@ import worlds.sms.dolphin.bit_helper as bit_helper
 import collections
 import asyncio
 
-nozzles_available = {"Hover Nozzle": False, "Rocket Nozzle": False, "Turbo Nozzle": False}
+ap_nozzles_received = ["Spray Nozzle"]
+in_game_nozzles_avail = ["Spray Nozzle"]
 
 
 def refresh_item_count(ctx, item_id, targ_address):
@@ -19,22 +20,14 @@ def refresh_collection_counts(ctx):
 
 
 def enable_nozzle(nozzle_name):
-    if nozzle_name == "Rocket Nozzle":
-        dme.write_double(addresses.SMS_ROCKET_UNLOCK, addresses.SMS_ROCKET_UNLOCK_VALUE)
-        return
-    elif nozzle_name == "Turbo Nozzle":
-        dme.write_double(addresses.SMS_TURBO_UNLOCK, addresses.SMS_TURBO_UNLOCK_VALUE)
-        return
-    elif nozzle_name == "Hover Nozzle":
-        dme.write_double(addresses.SMS_NOZZLE_LOCK_ADDRESS, addresses.SMS_NOZZLE_RELEASE)
-        return
+
+    return
 
 
 async def disable_nozzle(nozzle_name):
-    while not nozzles_available["Hover Nozzle"]:
+    while not ap_nozzles_received.__contains__("Hover Nozzle"):
         if nozzle_name == "Hover Nozzle":
-            dme.hook()
-            dme.write_bytes(addresses.SMS_NOZZLE_LOCK_ADDRESS, bytes.fromhex(addresses.SMS_SPRAY_LOCK_VALUE))
+            dme.write_bytes(addresses.SMS_SECONDARY_NOZZLE_ADDRESS, bytes.fromhex(addresses.SMS_SPRAY_NOZZLE_VALUE))
         await asyncio.sleep(0.1)
 
 
@@ -45,12 +38,86 @@ def initialize_nozzles():
 def unpack_item(item, ctx):
     refresh_collection_counts(ctx)
     if item == 523001:
-        enable_nozzle("Hover Nozzle")
+        ap_nozzles_received.append("Hover Nozzle")
     elif item == 532002:
-        enable_nozzle("Rocket Nozzle")
+        ap_nozzles_received.append("Rocket Nozzle")
     elif item == 523003:
-        enable_nozzle("Turbo Nozzle")
+        ap_nozzles_received.append("Turbo Nozzle")
     elif item == 523013:
-        enable_nozzle("Yoshi")
+        ap_nozzles_received.append("Yoshi")
     elif item == 523000:
-        enable_nozzle("Spray Nozzle")
+        ap_nozzles_received.append("Spray Nozzle")
+
+
+def check_in_game_nozzles():
+    list.clear(in_game_nozzles_avail)
+
+    primary_nozzle = dme.read_double(addresses.SMS_PRIMARY_NOZZLE_ADDRESS)
+    if primary_nozzle == addresses.SMS_SPRAY_NOZZLE_VALUE:
+        in_game_nozzles_avail.append("Spray Nozzle")
+
+    secondary_nozzle = dme.read_double(addresses.SMS_SECONDARY_NOZZLE_ADDRESS)
+    if secondary_nozzle == addresses.SMS_HOVER_NOZZLE_VALUE:
+        in_game_nozzles_avail.append("Hover Nozzle")
+    elif secondary_nozzle == addresses.SMS_ROCKET_NOZZLE_VALUE:
+        in_game_nozzles_avail.append("Rocket Nozzle")
+    elif secondary_nozzle == addresses.SMS_TURBO_NOZZLE_VALUE:
+        in_game_nozzles_avail.append("Turbo Nozzle")
+    return
+
+
+def nozzle_assignment():
+    primary_nozzle = "None"
+    secondary_nozzle = "None"
+    if in_game_nozzles_avail.__contains__("Spray Nozzle"):
+        if ap_nozzles_received.__contains__("Spray Nozzle"):
+            primary_nozzle = "Spray Nozzle"
+
+    if in_game_nozzles_avail.__contains__("Hover Nozzle"):
+        if ap_nozzles_received.__contains__("Hover Nozzle"):
+            secondary_nozzle = "Hover Nozzle"
+        else:
+            secondary_nozzle = primary_nozzle
+
+    if in_game_nozzles_avail.__contains__("Rocket Nozzle"):
+        if ap_nozzles_received.__contains__("Rocket Nozzle"):
+            secondary_nozzle = "Rocket Nozzle"
+        elif ap_nozzles_received.__contains__("Hover Nozzle"):
+            secondary_nozzle = "Hover Nozzle"
+        else:
+            secondary_nozzle = primary_nozzle
+
+    if in_game_nozzles_avail.__contains__("Turbo Nozzle"):
+        if ap_nozzles_received.__contains__("Turbo Nozzle"):
+            secondary_nozzle = "Turbo Nozzle"
+        elif ap_nozzles_received.__contains__("Hover Nozzle"):
+            secondary_nozzle = "Hover Nozzle"
+        else:
+            secondary_nozzle = primary_nozzle
+
+    if not ap_nozzles_received.__contains__("Spray Nozzle"):
+        primary_nozzle = secondary_nozzle
+
+    return [primary_nozzle, secondary_nozzle]
+
+
+def set_nozzle_assignment(nozzle_name):
+    assign_id = "0"
+    if nozzle_name == "Spray Nozzle":
+        assign_id = addresses.SMS_SPRAY_NOZZLE_VALUE
+    elif nozzle_name == "Hover Nozzle":
+        assign_id = addresses.SMS_HOVER_NOZZLE_VALUE
+    elif nozzle_name == "Rocket Nozzle":
+        assign_id = addresses.SMS_ROCKET_NOZZLE_VALUE
+    elif nozzle_name == "Turbo Nozzle":
+        assign_id = addresses.SMS_TURBO_NOZZLE_VALUE
+    return assign_id
+
+
+def enforce_nozzles(primary_nozzle, secondary_nozzle):
+    primary_id = set_nozzle_assignment(primary_nozzle)
+    secondary_id = set_nozzle_assignment(secondary_nozzle)
+    dme.write_double(addresses.SMS_PRIMARY_NOZZLE_ADDRESS, primary_id)
+    dme.write_double(addresses.SMS_SECONDARY_NOZZLE_ADDRESS, secondary_id)
+
+    return
