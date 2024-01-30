3c62f215a9215acabaf585c4996c4df0faab85ff
Marina <philnschipper@gmail.com>
2024-01-30 07:04:02 -0500

    Shine ID fix

--- worlds/sms/dolphin/bit_helper.py
+++ worlds/sms/dolphin/bit_helper.py
@@ -1,10 +1,12 @@
 def extract_bits(input_byte, input_offset):
     byte_list = []
+    input_byte = change_endian(input_byte)
     temp = str(bin(input_byte))
     temp = str.removeprefix(temp, "0b")
     for x in range(0, len(temp)):
         if temp[x] == "1":
-            byte_list.append((input_offset * 8) + int(x))
+            byte_list.append(((input_offset + 1) * 8) - int(x))
+    print(byte_list)
     return byte_list
 
 
