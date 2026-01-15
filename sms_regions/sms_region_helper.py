bbe0883da446d6d5ef9715accb7f8ac802025046
SomeJakeGuy <somejakeguy@gmail.com>
2026-01-15 18:33:02 -0500

    Requirements now include a skip_forward field (with comments)

--- sms_regions/sms_region_helper.py
+++ sms_regions/sms_region_helper.py
@@ -28,7 +28,7 @@ class Requirements(NamedTuple):
     blue_coins: Optional[int] = None
     location: Optional[str] = None
     corona: bool = False  # is corona access needed (configurable)
-    fluddless: bool = None
+    skip_forward: bool = None # Only in logic if tickets / fluddless are true
 
 
 class Shine(NamedTuple):
