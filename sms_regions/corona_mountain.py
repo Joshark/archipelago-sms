ce2243974cdf20de86345a374c08da19ae59b77b
SomeJakeGuy <somejakeguy@gmail.com>
2026-01-27 01:48:39 -0500

    Added Corona Mountain.

---
+++ sms_regions/corona_mountain.py
@@ -0,0 +1,17 @@
+from .sms_region_helper import *
+
+CORONA_MOUNTAIN: SmsRegion = SmsRegion(SmsRegionName.CORONA,
+    requirements=[Requirements(SPRAY_AND_HOVER)],
+    shines=[],
+    blue_coins=[BlueCoin("Platform", [Requirements([[NozzleType.hover]])]),
+        BlueCoin("Back Right Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Left Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Front Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Front Left Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Front Right Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Back Left Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Far Back Left Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Far Back Right Lava", [Requirements(SPRAY_AND_HOVER)]),
+        BlueCoin("Right Lava", [Requirements(SPRAY_AND_HOVER)])],
+    nozzle_boxes=[NozzleBox("Rocket Box", [Requirements(SPRAY_AND_HOVER)])],
+    parent_region=SmsRegionName.PLAZA)
