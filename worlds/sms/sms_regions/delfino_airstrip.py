576918faf142f1e72f259eb7f43984bfc058f908
SomeJakeGuy <somejakeguy@gmail.com>
2026-01-28 19:58:53 -0500

    removed TODO comment.

--- worlds/sms/sms_regions/delfino_airstrip.py
+++ worlds/sms/sms_regions/delfino_airstrip.py
@@ -1,7 +1,6 @@
 from .sms_region_helper import *
 
 # Delfino Airstrip
-# TODO Reminder that corona=True means that game must be able to reach Corona mountain
 DELFINO_AIRSTRIP: SmsRegion = SmsRegion(SmsRegionName.AIRSTRIP,
     shines=[Shine("Delfino Airstrip Dilemma", [Requirements(ANY_SPLASHER, skip_forward=False)]),
         Shine("Red Coin Waterworks", [Requirements([[NozzleType.turbo]], corona=True)])],
