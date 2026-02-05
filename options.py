c8ba23c986d9d2ba47fa4b413cd707c60faeed34
SomeJakeGuy <somejakeguy@gmail.com>
2026-02-05 05:09:51 -0500

    Introduced max required percentage to be 90 or 95% based on location count available after placing other progression items.

--- options.py
+++ options.py
@@ -21,7 +21,7 @@ class EnableCoinShines(Toggle):
 # Several locations to place filler/move around.
 class CoronaMountainShines(Range):
     """How many Shine Sprites are required to access Corona Mountain and the Delfino Airstrip revisit.
-    If less than this number of Shines exist in the pool, it will be adjusted to be 95% of the rest of locations available
+    If this is more than the locations available, This will be automatically adjusted to 90-95% of locations available
     after all other progression items are created / accounted for."""
     display_name = "Corona Mountain Shines"
     range_start = 0
