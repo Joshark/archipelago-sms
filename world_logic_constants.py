69b22dd063cbc96642cbe4d5f3ab264fc2cc22f5
SomeJakeGuy <somejakeguy@gmail.com>
2026-02-10 12:16:39 -0500

    New world_logic_constants.py file added. Changed max amount of blue coins to be lowered in some situations and raise trade shines required in others. Changed some debug messages, and left some TODO's about code that may not stay

---
+++ world_logic_constants.py
@@ -0,0 +1,4 @@
+# Maximum number of extra blue coins to create to not flood the pool with too many progression items for no reason.
+MAXIMUM_BLUE_COIN_PERCENTAGE: float = 1.2 # This is whatever the current amount is, + 20%
+
+# Mini
